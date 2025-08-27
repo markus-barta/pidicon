dbf54e9e21802e9ec0848584b5c22f4dd5502fc2
Markus Barta <markus@barta.com>
2025-08-27 17:13:34 +0200

    Update ESLint configuration to change source type to 'script' and adjust console rule severity
    This commit modifies the ESLint configuration by changing the parser option's source type from 'module' to 'script' and updates the 'no-console' rule to issue a warning instead of an error. These changes aim to enhance flexibility in code execution and improve developer experience during linting.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@
 module.exports = {
     root: true,
     env: { es2022: true, node: true },
-    parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+    parserOptions: { ecmaVersion: "latest", sourceType: "script" },
     extends: [
       "eslint:recommended",
       "plugin:import/recommended",
@@ -10,7 +10,7 @@ module.exports = {
     ],
     plugins: ["import", "promise"],
     rules: {
-      "no-console": ["error", { allow: ["warn", "error"] }],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
       "complexity": ["error", 10],
       "max-params": ["error", 5],
       "max-lines-per-function": [
